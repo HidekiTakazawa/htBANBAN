5a0c6ba69a54798c70f4cde8c2e1b371798b682a
HidekiTakazawa <ht0428a30@gmail.com>
2025-11-15 16:40:39 +0900

    servise-workerキャッシュ名の変更

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュするファイルの名前とバージョンを定義
-const CACHE_NAME = 'chinese-app-showcase-v1';
+const CACHE_NAME = 'chinese-app-showcase-v2';
 // キャッシュするファイルのリスト
 const urlsToCache = [
   './', // index.html を示す
