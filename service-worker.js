1c9e37d7e43a97cc5cd2044b3d7a6b89e2de483b
HidekiTakazawa <ht0428a30@gmail.com>
2025-11-17 17:57:16 +0900

    pwatest final

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュするファイルの名前とバージョンを定義
-const CACHE_NAME = 'chinese-app-showcase-v16';
+const CACHE_NAME = 'chinese-app-showcase-v17';
 // キャッシュするファイルのリスト
 const urlsToCache = [
   './', // index.html を示す
