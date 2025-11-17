785c0c2e4b16994997996df9c0fbbd41b5472580
HidekiTakazawa <ht0428a30@gmail.com>
2025-11-17 16:40:27 +0900

    PWAバグ訂正

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュするファイルの名前とバージョンを定義
-const CACHE_NAME = 'chinese-app-showcase-v9';
+const CACHE_NAME = 'chinese-app-showcase-v10';
 // キャッシュするファイルのリスト
 const urlsToCache = [
   './', // index.html を示す
