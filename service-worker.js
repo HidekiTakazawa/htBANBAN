2b3b47913b164c78fa8eaccff3ac6fc6a60574ae
HidekiTakazawa <ht0428a30@gmail.com>
2025-11-17 18:44:05 +0900

    pwa final21

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // キャッシュするファイルの名前とバージョンを定義
-const CACHE_NAME = 'chinese-app-showcase-v20';
+const CACHE_NAME = 'chinese-app-showcase-v21';
 // キャッシュするファイルのリスト
 const urlsToCache = [
   './', // index.html を示す
